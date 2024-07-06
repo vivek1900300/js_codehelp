43c65c7dc5ec95acfc3210ecea2c4c12c2ecd6b4
Vivek Kumar <167867477+vivek1900300@users.noreply.github.com>
2024-07-06 10:19:36 +0000

    JS Basic-1

---
+++ Basic_01/index.js
@@ -0,0 +1,128 @@
+// Variable
+
+// let a=5;
+// console.log(a);
+
+// let lastName = 'babbar';
+// console.log(lastName);
+
+// lastName = '3';
+// console.log(lastName);
+
+
+
+// reference type
+
+// Objects
+// Arrays
+
+// Operator
+
+// Arithmetic
+
+// let a = 4;
+// let b = 6;
+ 
+// let c = a+b;
+
+// console.log(c);
+
+// pre /post
+
+// ++x Pre Increment 
+    //    Firstly Increment the value  secondly the use the Value
+
+
+// x++ Post Increment
+    //  use the value then increment the value
+
+// --x pre decrement    
+    // decrement the value then use the value
+
+//  x-- post decrement
+    // use the value then decrment the value
+
+// Comparison operator
+//    === -strict equality
+
+// loose Equality
+//   ==
+
+//  Strict equality
+//   ===
+
+// Ternary Operator
+
+// let age = 27;
+
+// let Status = (age >=18) ? 'yes vote' : 'no vote';
+
+// console.log(Status);
+
+
+// Logical Operator
+
+// Bitwise Operator
+
+// Control statement
+// if-else
+// switch
+
+// let marks = 59;
+
+// if(marks > 90){
+//     console.log('a');
+// }
+// else if(marks > 80){
+//     console.log('b');
+// }
+// else if(marks > 70){
+//     console.log('c');
+// }
+// else {
+//     console.log('e');
+// }
+
+// Switch
+
+// let num = 6;
+
+// switch(num){
+//     case 1: console.log('a');
+//     break;
+//     case 2: console.log('b');
+//     break;
+//     case 3: console.log('c');
+//     break;
+//     default: console.log('r');
+// }
+
+
+
+// loops
+
+// for loop
+
+// for(let i=2; i<6; i=i+1){
+
+// console.log(i);}
+
+// while ioop
+
+// let x = 1;
+// while(x < 5){
+
+//     console.log(x);
+//     x++;
+// }
+
+// do while loop
+
+// let x = 1;
+
+// do {
+
+//     console.log(x);
+//     x++;
+// }
+// while (x < 7);
