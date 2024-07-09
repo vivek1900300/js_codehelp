6eedb9d97391c64939b6d4944d111e63609fede0
Vivek Kumar <167867477+vivek1900300@users.noreply.github.com>
2024-07-09 15:50:37 +0000

    basic_04

---
+++ Basic_04/Index.js
@@ -0,0 +1,77 @@
+// Function
+
+//  function declaration
+// function run(){
+//       console.log('running');
+
+// }
+// //function call or invoke
+
+// run();
+
+// // Named function assignment
+// let stand = function walk(){
+//     console.log('walking');
+// }
+// stand();
+
+// let jump = stand;
+
+// jump();
+
+// //anonymous function assignment
+
+// let play = function(){
+//     console.log('playing');
+// }
+// play();
+
+//rest operator
+
+// function sum(num, value, ...args){
+//    console.log(args);
+// }
+// sum(1,2,3,4,5);
+
+// Default Parameters
+
+
+//getter setter
+
+//getter->access properties
+//setter->change and mulate properties
+
+
+// try and catch blocks <-- error handling
+
+//scope
+
+// {
+//     var a = 5;
+
+//     // console.log(a);
+// }
+// console.log(a);
+
+// function walk(){
+//     var a = 5;
+// }
+// console.log(a);
+
+// for(var i=0; i<=5; i++){
+
+// }
+// console.log(i);
+
+// 
+// if(true){
+//     var a = 5;
+
+// }
+// console.log(a);
+
+
+//reducing an array
+
+
+
